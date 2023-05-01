c3abc5022479e7ab2e47097fe25b01b3cc6a64df
mayurwab <bodlexnig@outlook.com>
2023-05-01 05:47:51 +0100

    MOD: Fixed create.sh and Created crud test script

--- create.sh
+++ create.sh
@@ -2,55 +2,65 @@
 
 BUCKETNAME="crud-serverless-api-bucket-tut"
 
-echo "Compressing Functions...."
+if [[ -z $1 || -z $2 || -z $3 || -z $4 ]]; then
 
-sleep 2
+    echo "No Arguments Passed. Arguments must be exactly 4. Try: create, read, update, delete"
 
-cd $(pwd)/lambda-functions/${1}/
-zip ${1}.zip ${1}-function.py
+    exit 1
 
-cd -
+else
+  
+    echo "Compressing Functions...."
 
-cd $(pwd)/lambda-functions/${2}/
-zip ${2}.zip ${2}-function.py
+    sleep 2
 
-cd -
+    cd $(pwd)/lambda-functions/${1}/
+    zip ${1}.zip ${1}-function.py
 
-cd $(pwd)/lambda-functions/${3}/
-zip ${3}.zip ${3}-function.py
+    cd -
 
-cd -
+    cd $(pwd)/lambda-functions/${2}/
+    zip ${2}.zip ${2}-function.py
 
-cd $(pwd)/lambda-functions/${4}/
-zip ${4}.zip ${4}-function.py
+    cd -
 
-cd -
+    cd $(pwd)/lambda-functions/${3}/
+    zip ${3}.zip ${3}-function.py
 
-read -p "Enter Your Region: " REGION
+    cd -
 
-echo "Creating S3 Bucket..."
+    cd $(pwd)/lambda-functions/${4}/
+    zip ${4}.zip ${4}-function.py
 
-sleep 2
+    cd -
 
-aws s3api create-bucket --bucket $BUCKETNAME --region $REGION
+    read -p "Enter Your Region: " REGION
 
-echo "Copying Function Artifacts to S3 Bucket..."
+    echo "Creating S3 Bucket..."
 
-sleep 2
+    sleep 2
 
-aws s3 cp $(pwd)/lambda-functions/${1}/${1}.zip  s3://$BUCKETNAME/v1.0.0/${1}.zip
+    aws s3api create-bucket --bucket $BUCKETNAME --region $REGION
 
-aws s3 cp $(pwd)/lambda-functions/${2}/${2}.zip  s3://$BUCKETNAME/v1.0.0/${2}.zip
+    echo "Copying Function Artifacts to S3 Bucket..."
 
-aws s3 cp $(pwd)/lambda-functions/${3}/${3}.zip  s3://$BUCKETNAME/v1.0.0/${3}.zip
+    sleep 2
 
-aws s3 cp $(pwd)/lambda-functions/${4}/${4}.zip  s3://$BUCKETNAME/v1.0.0/${4}.zip
+    aws s3 cp $(pwd)/lambda-functions/${1}/${1}.zip  s3://$BUCKETNAME/v1.0.0/${1}.zip
 
-echo "Creating Infrastructure..."
+    aws s3 cp $(pwd)/lambda-functions/${2}/${2}.zip  s3://$BUCKETNAME/v1.0.0/${2}.zip
 
-sleep 2
+    aws s3 cp $(pwd)/lambda-functions/${3}/${3}.zip  s3://$BUCKETNAME/v1.0.0/${3}.zip
 
-terraform init
-terraform apply -auto-approve
+    aws s3 cp $(pwd)/lambda-functions/${4}/${4}.zip  s3://$BUCKETNAME/v1.0.0/${4}.zip
 
-echo "DONE"
+    echo "Creating Infrastructure..."
+
+    sleep 2
+
+    terraform init
+    terraform apply -auto-approve
+
+    echo "DONE"
+
+fi
